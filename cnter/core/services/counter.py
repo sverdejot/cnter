933003ae29ef0129787f7559e0626a024b89f0ec
sverdejot <samularoda@gmail.com>
2022-07-28 01:23:07 +0200

    #DEV-03 Fully working CRUD+ on counter, based on schemas

--- cnter/core/services/counter.py
+++ cnter/core/services/counter.py
@@ -21,7 +21,7 @@ def create_counter(alias: str, private: bool, owner: UserSchema) -> CounterSchem
     }
     with db.driver.session() as session:
         result = session.run(cypher_query, parameters={
-            'id': owner.id,
+            'id': str(owner.id),
             'props': props
         })
         if node := result.single():
@@ -32,7 +32,7 @@ def create_counter(alias: str, private: bool, owner: UserSchema) -> CounterSchem
 
 def join_counter(user: UserSchema, counter: CounterSchema) -> dict:
     join_query = (
-        "MATCH (user:User {uid: $uid}), (counter:Counter {cid: $cid})\n"
+        "MATCH (user:User {id: $uid}), (counter:Counter {id: $cid})\n"
         "CREATE (user)-[belong:BELONGS {joined: timestamp(), ts: $ts}]->(counter)\n"
         "RETURN user, belong, counter"
     )
@@ -40,8 +40,8 @@ def join_counter(user: UserSchema, counter: CounterSchema) -> dict:
         result = session.run(
             join_query,
             parameters={
-                'cid': counter.id,
-                'uid': user.id,
+                'cid': str(counter.id),
+                'uid': str(user.id),
                 'joined': datetime.now().timestamp(),
                 'ts': []
             }
@@ -49,67 +49,68 @@ def join_counter(user: UserSchema, counter: CounterSchema) -> dict:
         if node := result.single():
             return BelongshipSchema(
                 user=UserSchema(**node.data().get('user')),
-                counter=CounterSchema(**node.data().get('counter')),
-                ts=node.data().get('counter').get('joined')
+                counter=CounterSchema(**node.data().get('counter'), owner=UserSchema(**node.data().get('user'))),
+                ts=[float(ts) for ts in node.get('belong').get('ts')],
+                joined=float(node.get('belong').get('joined'))
             )
         else:
             logging.warn("No return data was found on join")
 
-def update_counter(username: str, alias: str) -> dict:
-    # TODO: save ts of update
-    user_belong_query = (
-        "MATCH (user:User)-[:BELONGS]->(counter:Counter)\n"
-        "WHERE user.username = $username and counter.alias = $alias RETURN counter"
-    )
-
-    update_counter_query = (
-        "MATCH (counter:Counter)\n"
-        "WHERE counter.alias = $alias\n"
-        "SET counter.status = $new_status\n"
-        "RETURN counter"
+def update_counter(belongship: BelongshipSchema) -> BelongshipSchema:
+    cypher_query = (
+        "MATCH (user:User)-[belong:BELONGS]->(counter:Counter)\n"
+        "WHERE user.id = $uid and counter.id = $cid\n"
+        "SET counter.status = counter.status + 1\n"
+        "SET belong.ts = belong.ts + [$ts]\n"
+        "RETURN user, belong, counter"
     )
 
     with db.driver.session() as session:
         result = session.run(
-            user_belong_query, parameters={
-                'username': username, 
-                'alias': alias
+            cypher_query, parameters={
+                'uid': str(belongship.user.id), 
+                'cid': str(belongship.counter.id),
+                'ts': float(datetime.now().timestamp())
             })
 
-        if counter := result.single():
-            result = session.run(
-                update_counter_query,
-                parameters={
-                    'alias': counter.get('counter').get('alias'),
-                    'new_status': counter.get('counter').get('status') + 1
-                })
-            if node := result.single():
-                logging.info(f"User [{username}] updated counter [{node.get('counter').get('alias')}]")
-                return node.data()
+        if node := result.single():
+            return BelongshipSchema(
+                ts=node.get('belong').get('ts'),
+                joined=node.get('belong').get('joined'),
+                user=UserSchema(**node.data().get('user')),
+                counter=CounterSchema(**node.data().get('counter'), owner=UserSchema(**node.data().get('user')))
+            )
+            
 
-def delete_counter(username: str, alias: str) -> dict:
+def delete_counter(counter: CounterSchema) -> bool:
     delete_query = (
-        "MATCH (user:User {username: $username})-[owner:OWNS]->(counter:Counter {alias: $alias})\n"
-        "DELETE owner, counter"
+        "MATCH (counter:User {id: $cid})\n"
+        "DETACH DELETE counter"
     )
 
     with db.driver.session() as session:
         result = session.run(delete_query, parameters={
-            'username': username,
-            'alias': alias
+            'cid': counter.id
         })
         if result.single():
             logging.info(f"Succesfully deleted")
+            return True
+        else:
+            return False
 
 
-def get_by_alias(alias: str) -> dict:
+def get_by_alias(alias: str) -> CounterSchema:
     get_query = (
-        "MATCH (counter:COUNTER)\n"
-        "WHERE counter.alias = $alias"
+        "MATCH (user:User)-[:OWNS]->(counter:Counter)\n"
+        "WHERE counter.alias = $alias RETURN counter, user"
     )
     with db.driver.session() as session:
         result = session.run(get_query, parameters={
             'alias': alias
         })
 
-        return result.single().data()
+        if node := result.single():
+            return CounterSchema(
+                **node.data().get('counter'),
+                owner=UserSchema(**node.data().get('user'))
+            )
