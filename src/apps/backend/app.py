c892ee772488be6c35271382d18142f4ef5bc157
sverdejot <sverdejot@gmail.com>
2023-01-06 21:19:58 +0100

    👾 MUpdate README.md and minor changes

--- src/apps/backend/app.py
+++ src/apps/backend/app.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 from routers import counters
 
-app = FastAPI()
+app = FastAPI(
+    title='🔢 cnter',
+    description='a counter-based social media app',
+    version='0.1.0',
+    contact={
+        'name': 'Samuel Verdejo',
+        'email': 'contacto@sverdejot.dev',
+        'url': 'www.sverdejot.dev',
+    }
+)
 
 app.include_router(counters.router)
